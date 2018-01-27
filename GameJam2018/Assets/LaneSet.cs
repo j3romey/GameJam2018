9b68c0879437aaa92a66fb72779d28998c1d5819
j3romey <jerome.adviento@hotmail.com>
2018-01-27 01:35:45 -0700

    can make song mappings

---
+++ GameJam2018/Assets/LaneSet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneSet : MonoBehaviour {
+
+    public List<Lane> lanes;
+    public int dropSpeed;
+    public float space;
+    float curSpace;
+    public float height;
+
+    [System.Serializable]
+    public class Lane
+    {
+        public GameObject dropPrefab;
+
+        public List<GameObject> cubes;
+
+        public Vector3 location;
+
+        public Lane()
+        {
+            cubes = new List<GameObject>();
+        }
+
+        public void Spawn(){
+            Instantiate(dropPrefab, location, Quaternion.identity);   
+        }
+    }
+
+    void Awake()
+    {
+        curSpace = space * -lanes.Count + space;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        float f = space * -lanes.Count + space;
+
+        for (int i = 0; i < lanes.Count; i++){
+            lanes[i].location = (Vector3.right * f + Vector3.up * height);
+            f += 2 * space;
+        }
+
+
+	}
+}
---
+++ GameJam2018/Assets/Music.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicTimer : MonoBehaviour {
+
+    [SerializeField] KeyCode button;
+    AudioSource audio;
+
+
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(button)){
+            Debug.Log(audio.time);
+        }	
+	}
+}
