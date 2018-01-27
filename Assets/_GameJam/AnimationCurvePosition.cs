d0606b584f5b3631f321f125ba72dbab6e9e90d2
modanhan <modanhan@live.com>
2018-01-27 02:49:54 -0700

    graphics set up

---
+++ Assets/_GameJam/AnimationCurvePosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCurvePosition : MonoBehaviour
+{
+
+    public AnimationCurve animationCurve;
+    public float t;
+
+    public Vector3 start, end;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(start, end, animationCurve.Evaluate(t));
+        t += Time.deltaTime;
+    }
+}
