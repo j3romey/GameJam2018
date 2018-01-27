02acf820bbf9701a899898d2dc080ec2d27da92e
modanhan <modanhan@live.com>
2018-01-27 16:59:38 -0700

    text scrambling, more effects

---
+++ Assets/_GameJam/Effects/TextureShift.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureShift : MonoBehaviour
+{
+
+    public Material material;
+    public Vector2 shift;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var a = material.mainTextureOffset;
+        a += shift * Time.deltaTime;
+        a.x = Mathf.Repeat(a.x, 1);
+        a.y = Mathf.Repeat(a.y, 1);
+        material.mainTextureOffset = a;
+    }
+}
--- Assets/_GameJam/ScrambleText.cs
+++ Assets/_GameJam/ScrambleText.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,7 +14,7 @@ public class ScrambleText : MonoBehaviour
     [TextArea(10, 20)]
     public string normal, scrambled;
 
-    public string ans;
+    public StringBuilder ans;
 
     public bool debug;
 
@@ -24,10 +25,8 @@ public class ScrambleText : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        ans = "";
         currentDrop = 0;
         currentChar = 0;
-        text.text = ans;
         dropsPerChar = totalDrops / normal.Length;
         longerChars = totalDrops % normal.Length;
         scores = new int[normal.Length];
@@ -35,7 +34,19 @@ public class ScrambleText : MonoBehaviour
         for (int i = 0; i < prefixSum.Length; i++) prefixSum[i] = dropsPerChar;
         for (int i = 0; i < longerChars; i++) prefixSum[i]++;
         for (int i = 1; i < prefixSum.Length; i++) prefixSum[i] += prefixSum[i - 1];
-
+        ans = new StringBuilder();
+        for (int i = 0; i < normal.Length; i++)
+        {
+            if (char.IsLetter(normal[i]))
+            {
+                ans.Append((char)Random.Range((int)'a', (int)'z'));
+            }
+            else
+            {
+                ans.Append(normal[i]);
+            }
+        }
+        text.text = ans.ToString();
     }
 
     // Update is called once per frame
@@ -45,6 +56,18 @@ public class ScrambleText : MonoBehaviour
         {
             // if (Input.GetKeyDown(KeyCode.O)) Next(true);
             // else if (Input.GetKeyDown(KeyCode.P)) Next(false);
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                Decide(0);
+            }
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                Decide(1);
+            }
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                Decide(2);
+            }
         }
     }
 
@@ -60,11 +83,23 @@ public class ScrambleText : MonoBehaviour
     {
         if (currentChar >= normal.Length) return;
         scores[currentChar] += grades[x];
+        if (char.IsLetter(normal[currentChar]))
+        {
+            if (scores[currentChar] >= dropsPerChar)
+            {
+                ans[currentChar] = normal[currentChar];
+            }
+            else
+            {
+                ans[currentChar] = (char)Random.Range((int)'a', (int)'z');
+            }
+        }
         currentDrop++;
         if (currentDrop > prefixSum[currentChar])
         {
+
             currentChar++;
         }
-        text.text = ans;
+        text.text = ans.ToString();
     }
 }
