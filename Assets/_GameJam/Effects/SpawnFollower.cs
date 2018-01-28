fa450fc6ad0986f255b274a64ea74a5c62e290e5
modanhan <modanhan@live.com>
2018-01-27 21:43:38 -0700

    trails slowly disappear

---
+++ Assets/_GameJam/Effects/SpawnFollower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFollower : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public GameObject instance;
+
+    // Use this for initialization
+    void Start()
+    {
+        instance = Instantiate(prefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        instance.transform.position = transform.position;
+    }
+}
