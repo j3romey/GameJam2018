48ae6837628f696d35e480b420ec006adc6d8028
modanhan <modanhan@live.com>
2018-01-28 14:19:59 -0700

    texts

--- Assets/_GameJam/LifeTimer.cs
+++ Assets/_GameJam/LifeTimer.cs
@@ -32,65 +32,75 @@ public class LifeTimer : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (life <= 0) { 
-            
-            if(type == Type.Key){
+        if (life <= 0)
+        {
+
+            if (type == Type.Key)
+            {
                 if (Miss) Instantiate(Miss, transform.position, transform.rotation);
                 keyState = State.Miss;
-                TestText.instance.Write(keyState.ToString());
+                TestText.instance.Write(keyState.ToString() + " x_x");
                 ScrambleText.instance.Decide(2);
                 ComboTracker.instance.counter = 0;
                 ComboTracker.instance.Change();
             }
-           
+
             // send to text
-            Destroy(gameObject); 
+            Destroy(gameObject);
         }
         life -= Time.deltaTime;
 
 
     }
 
-    public void Hit(State state){
+    public void Hit(State state)
+    {
 
-        if(state == State.Miss){
+        if (state == State.Miss)
+        {
             ComboTracker.instance.counter = 0;
-        }else{
+        }
+        else
+        {
             ComboTracker.instance.counter++;
         }
 
         ComboTracker.instance.Change();
-        
-        switch(state){
+
+        switch (state)
+        {
             case State.Perfect:
                 if (Perfect) Instantiate(Perfect, transform.position, transform.rotation);
                 ScrambleText.instance.Decide(0);
+                TestText.instance.Write(state.ToString() + "!! :D");
                 break;
             case State.Good:
                 if (Good) Instantiate(Good, transform.position, transform.rotation);
                 ScrambleText.instance.Decide(1);
+                TestText.instance.Write(state.ToString());
                 break;
             case State.Ok:
                 if (Ok) Instantiate(Ok, transform.position, transform.rotation);
                 ScrambleText.instance.Decide(1);
+                TestText.instance.Write(state.ToString() + " :)");
 
                 break;
             case State.Miss:
                 if (Miss) Instantiate(Miss, transform.position, transform.rotation);
                 ScrambleText.instance.Decide(2);
-           
+                TestText.instance.Write(state.ToString() + " X_X");
+
                 break;
             default:
                 break;
         }
         keyState = state;
-        TestText.instance.Write(keyState.ToString());
         // send to text
         Destroy(gameObject);
     }
